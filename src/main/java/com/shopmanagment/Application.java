f4efe2b041363efa5039275a9974c8d196ff3340
Pulsha Andrei <ap.ttnby@gmail.com>
2016-12-18 03:43:21 +0300

    task: options method

--- src/main/java/com/shopmanagment/Application.java
+++ src/main/java/com/shopmanagment/Application.java
@@ -2,10 +2,22 @@ package com.shopmanagment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.DispatcherServletAutoConfiguration;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
 
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        super.onStartup(servletContext);
+        servletContext.getServletRegistration(DispatcherServletAutoConfiguration.DEFAULT_DISPATCHER_SERVLET_BEAN_NAME)
+                .setInitParameter("dispatchOptionsRequest", "true");
     }
 }
